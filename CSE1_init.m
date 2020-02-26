0330661bb95f2dadf3e30e732c3570ef07bd0ae2
michoy <michaelhoyer@outlook.com>
2020-02-26 16:36:08 +0100

    adds missing rotation block in observer

--- CSE1_init.m
+++ CSE1_init.m
@@ -24,11 +24,12 @@ x_init = zeros(1,9);
 %% Constant observer matrices
 
 % Gain matrix L (tuning parameters)
-L1 = diag([75 10 10]);
-L2 = diag([75 10 10]);
-L3 = diag([7.5 1 1]);
-
-L = [L1; M\L2; L3];
+% L1 = diag([1 1 1]);
+% L2 = diag([0.1 0.1 0.1]);
+% L3 = diag([0.025 0.01 0.0001]);
+L1 = diag([10 10 10]);
+L2 = diag([10 10 10]);
+L3 = diag([1 0.3 1]);
 
 % Input matrix B
 B = [zeros(3); inv(M); zeros(3)];
