40fa41e871af8100505702228a5735b4193575a5
daliacasanova <daliacasanova@hotmail.com>
2020-02-27 18:14:32 +0100

    <control_custom>

--- CSE1_init.m
+++ CSE1_init.m
@@ -27,9 +27,9 @@ x_init = zeros(1,9);
 % L1 = diag([1 1 1]);
 % L2 = diag([0.1 0.1 0.1]);
 % L3 = diag([0.025 0.01 0.0001]);
-L1 = diag([10 10 10]);
-L2 = diag([10 10 10]);
-L3 = diag([1 0.3 1]);
+L1 = diag([1 1 1]);
+L2 = diag([0.1 0.1 0.1]);
+L3 = diag([0.025 0.01 0.0001]);
 
 % Input matrix B
 B = [zeros(3); inv(M); zeros(3)];
