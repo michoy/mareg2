7797cb43e19fe98d65b434b6b801d94393bd5146
michoy <michaelhoyer@outlook.com>
2020-04-02 11:17:21 +0200

    enables plotting without export

--- plotting.m
+++ plotting.m
@@ -1,23 +1,24 @@
-x = log{11}.Values.Data(1,:);
-y = log{11}.Values.Data(2,:);
-psi = log{11}.Values.Data(3,:);
+x = logsout{13}.Values.Data(1,:);
+y = logsout{13}.Values.Data(2,:);
+psi = logsout{13}.Values.Data(3,:);
 
-x_hat = log{9}.Values.Data(:,1);
-y_hat = log{9}.Values.Data(:,2);
-psi_hat = log{9}.Values.Data(:,3);
+x_hat = logsout{10}.Values.Data(:,1);
+y_hat = logsout{10}.Values.Data(:,2);
+psi_hat = logsout{10}.Values.Data(:,3);
 
-x_d = log{5}.Values.Data(:,1);
-y_d = log{5}.Values.Data(:,2);
-psi_d = log{5}.Values.Data(:,3);
+x_d = logsout{5}.Values.Data(:,1);
+y_d = logsout{5}.Values.Data(:,2);
+psi_d = logsout{5}.Values.Data(:,3);
 
-tau_cmd_x = log{4}.Values.Data(1,:);
-tau_cmd_y = log{4}.Values.Data(2,:);
-tau_cmd_psi = log{4}.Values.Data(3,:);
+tau_cmd_x = logsout{2}.Values.Data(1,:);
+tau_cmd_y = logsout{2}.Values.Data(2,:);
+tau_cmd_psi = logsout{2}.Values.Data(3,:);
 
 subplot(3,1,1)
 hold on
 plot(x, y, x_hat, y_hat, x_d, y_d)
-legend('eta', 'eta\_hat', 'eta\_d')
+lgd = legend('eta', 'eta\_hat', 'eta\_d');
+lgd.Location = 'southwest';
 grid
 axis equal
 hold off
@@ -27,7 +28,8 @@ hold on
 plot(psi)
 plot(psi_hat)
 plot(psi_d)
-legend('psi', 'psi\_hat', 'psi\_d')
+lgd = legend('psi', 'psi\_hat', 'psi\_d');
+lgd.Location = 'southwest';
 grid 
 hold off
 
@@ -36,6 +38,7 @@ hold on
 plot(tau_cmd_x)
 plot(tau_cmd_y)
 plot(tau_cmd_psi)
-legend('tau\_cmd\_x', 'tau\_cmd\_y', 'tau\_cmd\_psi')
+lgd = legend('tau\_cmd\_x', 'tau\_cmd\_y', 'tau\_cmd\_psi');
+lgd.Location = 'southwest';
 grid
 hold off
